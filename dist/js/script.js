70d98320eed0906c51d47f2caab07a71da6721c9
ksenia_kap_1 <kseniakap096@gmail.com>
2024-04-30 17:45:59 +0300

    1 часть

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+function toggleNavigationContent() {
+  var navigationContent = document.getElementById("navigationContent");
+  if (navigationContent.style.display === "none") {
+    navigationContent.style.display = "block";
+  } else {
+    navigationContent.style.display = "none";
+  }
+}
