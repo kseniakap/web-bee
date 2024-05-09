f150dace5463d5987c10e2cef2f1a798da44d0ab
ksenia_kap_1 <kseniakap096@gmail.com>
2024-05-09 23:44:51 +0300

    Update 2024-05-09T20:44:46.508Z

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
-// подгрузка данных
+// Подгрузка данных
 function loadData(filename) {
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       document.getElementById("content").innerHTML = this.responseText;
+
+      // Извлечение и подключение скрипта из разметки
+      let scriptTags = document
+        .getElementById("content")
+        .getElementsByTagName("script");
+      let loadedScripts = {};
+
+      for (let i = 0; i < scriptTags.length; i++) {
+        let src = scriptTags[i].src;
+
+        if (src && !loadedScripts[src]) {
+          let script = document.createElement("script");
+          script.src = src;
+          loadedScripts[src] = true;
+          document.body.appendChild(script);
+        } else if (!src) {
+          let scriptCode = scriptTags[i].innerHTML;
+          let script = document.createElement("script");
+          script.innerHTML = scriptCode;
+          document.body.appendChild(script);
+        }
+      }
+
       history.pushState(
         { filename: filename },
         "Page " + filename,
@@ -26,49 +49,39 @@ window.onpopstate = function (event) {
     loadData(event.state.filename);
   }
 };
-
 loadData("01.html");
 
-// отображение карты
-function initMap() {
-  map = DG.map("map", {
-    center: [56.7455, 37.16039],
-    zoom: 13,
-  });
-
-  DG.marker([56.75319, 37.16734]).addTo(map);
-
-  document.getElementById("map").style.display = "block";
-  document.getElementById("preloader").style.display = "none";
-}
-
-DG.then(function () {
-  initMap();
+window.addEventListener("beforeunload", function () {
+  localStorage.removeItem("startTime");
 });
 
-// время нахождения на сайте
-function updateTimer() {
-  let startTime = localStorage.getItem('startTime');
-  if (!startTime) {
-      startTime = new Date().getTime(); 
-      localStorage.setItem('startTime', startTime);
+document.addEventListener("DOMContentLoaded", function () {
+  // время нахождения на сайте
+  function updateTimer() {
+    let startTime = localStorage.getItem("startTime");
+    if (!startTime) {
+      startTime = new Date().getTime();
+      localStorage.setItem("startTime", startTime);
+    }
+
+    setInterval(function () {
+      let currentTime = new Date().getTime(),
+        elapsedTime = currentTime - startTime,
+        hours = Math.floor((elapsedTime / (1000 * 60 * 60)) % 24),
+        minutes = Math.floor((elapsedTime / 1000 / 60) % 60),
+        seconds = Math.floor((elapsedTime / 1000) % 60);
+
+      document.getElementById("timer").innerText =
+        (hours < 10 ? "0" : "") +
+        hours +
+        ":" +
+        (minutes < 10 ? "0" : "") +
+        minutes +
+        ":" +
+        (seconds < 10 ? "0" : "") +
+        seconds;
+    }, 1000);
   }
 
-  setInterval(function() {
-      let currentTime = new Date().getTime();
-      let elapsedTime = currentTime - startTime;
-      let hours = Math.floor((elapsedTime / (1000 * 60 * 60)) % 24);
-      let minutes = Math.floor((elapsedTime / 1000 / 60) % 60);
-      let seconds = Math.floor((elapsedTime / 1000) % 60);
-      document.getElementById('timer').innerText = 
-          (hours < 10 ? "0" : "") + hours + ":" + 
-          (minutes < 10 ? "0" : "") + minutes + ":" + 
-          (seconds < 10 ? "0" : "") + seconds;
-  }, 1000);
-}
-
-updateTimer();
-
-window.addEventListener('beforeunload', function() {
-  localStorage.removeItem('startTime');
+  updateTimer();
 });
